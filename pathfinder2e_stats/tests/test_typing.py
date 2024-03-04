0747f83046b69d456e080fd3b0b44b0232d611b0
crusaderky <crusaderky@gmail.com>
2024-03-04 23:43:47 +0000

    damage types arithmetics

--- pathfinder2e_stats/tests/test_typing.py
+++ pathfinder2e_stats/tests/test_typing.py
@@ -1,32 +1,58 @@
 from __future__ import annotations
 
+import pytest
+
 from pathfinder2e_stats import Damage
+
+
+def test_damage_type_validation():
+    with pytest.raises(TypeError):
+        Damage(1, 6, 4)
+    with pytest.raises(TypeError):
+        Damage("fire", 1, 6, multiplier=True)
+    with pytest.raises(TypeError):
+        Damage("fire", True, 6)
+    with pytest.raises(TypeError):
+        Damage("fire", 1, 6, splash=1)
+    with pytest.raises(ValueError):
+        Damage("fire", 1, 6, rule="foo")
+    Damage("fire", 1, 6, multiplier=0.5)
+    Damage("fire", 1, 6, multiplier=2)
 
 
 def test_damage_type_str():
     d = Damage("fire", 1, 6)
-    assert str(d) == "1d6 fire damage"
+    assert str(d) == "1d6 fire"
 
     d = Damage("fire", 2, 6, 3)
-    assert str(d) == "2d6+3 fire damage"
+    assert str(d) == "2d6+3 fire"
 
     d = Damage("fire", 2, 6, -1)
-    assert str(d) == "2d6-1 fire damage"
+    assert str(d) == "2d6-1 fire"
 
     d = Damage("fire", 0, 0, 1)
-    assert str(d) == "1 fire damage"
+    assert str(d) == "1 fire"
 
     d = Damage("fire", 6, 6, 1, 2)
-    assert str(d) == "(6d6+1)x2 fire damage"
+    assert str(d) == "(6d6+1)x2 fire"
 
     d = Damage("fire", 6, 6, 1, 0.5)
-    assert str(d) == "(6d6+1)/2 fire damage"
+    assert str(d) == "(6d6+1)/2 fire"
 
     d = Damage("fire", 0, 0, 1, persistent=True)
-    assert str(d) == "1 persistent fire damage"
+    assert str(d) == "1 persistent fire"
 
     d = Damage("fire", 0, 0, 1, splash=True)
-    assert str(d) == "1 fire splash damage"
+    assert str(d) == "1 fire splash"
+
+    d = Damage("piercing", 2, 6, 4, deadly=8)
+    assert str(d) == "2d6+4 deadly d8 piercing"
+
+    d = Damage("piercing", 2, 6, 4, fatal=10)
+    assert str(d) == "2d6+4 fatal d10 piercing"
+
+    d = Damage("fire", 6, 6, rule="basic_save")
+    assert str(d) == "6d6 fire, with a basic saving throw"
 
 
 def test_damage_type_copy():
