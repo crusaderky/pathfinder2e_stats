9d8e424f9bfb0f4df60d52efb62695595082330f
crusaderky <crusaderky@gmail.com>
2024-03-18 01:06:29 +0000

    damage()

---
+++ pathfinder2e_stats/damage.py
@@ -0,0 +1,161 @@
+from __future__ import annotations
+
+from collections.abc import Collection, Hashable, Mapping
+from typing import cast
+
+import numpy as np
+import xarray
+from xarray import DataArray, Dataset, align, concat, where
+
+from pathfinder2e_stats.check import check
+from pathfinder2e_stats.damage_spec import AnyDamageSpec, ExpandedDamage
+from pathfinder2e_stats.dice import roll
+
+
+def damage(
+    check_outcome: Dataset,
+    damage_spec: AnyDamageSpec,
+    *,
+    weaknesses: Mapping[str, int] | DataArray | None = None,
+    resistances: Mapping[str, int] | DataArray | None = None,
+    immunities: Mapping[str, bool] | Collection[str] | DataArray | None = None,
+    persistent_damage_rounds: int = 3,
+    persistent_damage_DC: int = 15,
+    splash_damage_targets: int = 2,
+) -> Dataset:
+    out = check_outcome.copy(deep=False)
+    damage_spec = ExpandedDamage(damage_spec)
+    out.attrs["damage_spec"] = damage_spec.to_dict_of_str()
+
+    damages = {
+        name: _roll_damage(check_outcome.outcome, spec, dims)
+        for name, spec, dims in (
+            ("direct_damage", damage_spec.filter(), None),
+            ("splash_damage", damage_spec.filter(splash=True), None),
+            (
+                "persistent_damage",
+                damage_spec.filter(persistent=True),
+                {"persistent_round": persistent_damage_rounds},
+            ),
+        )
+        if spec
+    }
+    damages = dict(
+        zip(damages, align(*damages.values(), join="outer", copy=False, fill_value=0))
+    )
+    out.update(damages)
+
+    if "splash_damage" in out:
+        if "direct_damage" in out:
+            out["direct_damage"] += out["splash_damage"]
+        else:
+            out["direct_damage"] = out["splash_damage"]
+
+    weaknesses = _parse_weaknesses(weaknesses)
+    resistances = _parse_weaknesses(resistances)
+    immunities = _parse_weaknesses(immunities)
+    _, weaknesses, resistances, immunities = align(
+        out, weaknesses, resistances, immunities, join="left", copy=False, fill_value=0
+    )
+    immunities = immunities.astype(bool)
+    if weaknesses.any():
+        out["weaknesses"] = weaknesses
+    if resistances.any():
+        out["resistances"] = resistances
+    if immunities.any():
+        out["immunities"] = immunities
+
+    for k in ("direct_damage", "splash_damage", "persistent_damage"):
+        if k in out:
+            damage = out[k]
+            damage = damage.where(~immunities, 0)
+            damage = cast(DataArray, np.maximum(0, damage - resistances))
+            damage = damage + weaknesses.where(damage > 0, 0)
+            out[k] = damage
+
+    total_damage = []
+    if "direct_damage" in out:
+        total_damage.append(out["direct_damage"])
+    if "splash_damage" in out:
+        out.attrs["splash_damage_targets"] = splash_damage_targets
+        # Splash damage to main target is already included in direct damage
+        total_damage.append(out["splash_damage"] * (splash_damage_targets - 1))
+    if "persistent_damage" in out:
+        out["persistent_damage_check"] = check(
+            DC=persistent_damage_DC,
+            dims={"persistent_round": persistent_damage_rounds},
+            allow_critical_failure=False,
+            allow_critical_success=False,
+        ).outcome
+        out.attrs["persistent_damage_DC"] = persistent_damage_DC
+        out["apply_persistent_damage"] = (
+            out["persistent_damage_check"].cumsum("persistent_round")
+            # Check to extinguish persistent damage is done after taking it
+            .shift({"persistent_round": 1}, fill_value=0)
+            == 0
+        )
+        total_damage.append(
+            (out["persistent_damage"] * out["apply_persistent_damage"]).sum(
+                "persistent_round"
+            )
+        )
+
+    if total_damage:
+        out["total_damage"] = sum(total_damage).sum("damage_type")  # type: ignore[union-attr]
+    else:
+        out["total_damage"] = xarray.zeros_like(out["outcome"])
+
+    return out
+
+
+def _roll_damage(
+    check_outcome: DataArray, spec: ExpandedDamage, dims: dict[str, int] | None
+) -> DataArray:
+    out = []
+    for specs in spec.values():
+        damage_rolls = []
+        for d in specs:
+            r = roll(d.dice, d.faces, d.bonus, dims=cast(Mapping[Hashable, int], dims))
+
+            if d.multiplier == 2:
+                r *= 2
+            elif d.multiplier == 0.5:
+                r //= 2
+            else:
+                assert d.multiplier == 1
+
+            damage_rolls.append(r)
+
+        r = concat(damage_rolls, dim="damage_type", join="outer", fill_value=0)
+        del damage_rolls
+        r.coords["damage_type"] = [d.type for d in specs]
+        r = r.groupby("damage_type", squeeze=False).sum()
+        out.append(r)
+
+    out = list(align(*out, copy=False, join="outer", fill_value=0))
+    return sum(where(check_outcome == dos, r, 0) for dos, r in zip(spec, out))
+
+
+def _parse_weaknesses(a: Collection[str] | DataArray | None) -> DataArray:
+    if isinstance(a, DataArray):
+        pass
+    elif not a:
+        a = DataArray([], dims=["damage_type"], coords={"damage_type": []}).astype(int)
+    else:
+        if not isinstance(a, Mapping):
+            a = {k: True for k in a}  # immunities
+        a = DataArray(
+            list(a.values()),
+            dims=["damage_type"],
+            coords={"damage_type": list(a)},
+        )
+
+    if "damage_type" not in a.dims or (
+        a.sizes["damage_type"] and "damage_type" not in a.coords
+    ):
+        raise ValueError(
+            f"Expected DataArray with labelled dimension 'damage_type'; got {a}"
+        )
+    if a.dtype != bool and a.dtype.kind != "i":
+        raise ValueError(f"Expected DataArray with int or bool dtype; got {a}")
+    return a
---
+++ pathfinder2e_stats/tests/test_damage.py
@@ -0,0 +1,335 @@
+from __future__ import annotations
+
+import numpy as np
+import pytest
+from xarray import DataArray
+from xarray.testing import assert_equal
+
+from pathfinder2e_stats import Damage, check, damage, set_size
+
+
+def test_damage_simple():
+    actual = damage(check(16, DC=21), Damage("slashing", 1, 6, 3, deadly=8))
+    assert np.unique(actual.outcome).tolist() == [-1, 0, 1, 2]
+
+    assert actual.direct_damage.sizes == {"roll": 1000, "damage_type": 1}
+    assert actual.total_damage.sizes == {"roll": 1000}
+    assert actual.damage_type.values.tolist() == ["slashing"]
+    assert actual.direct_damage[actual.outcome == -1].sum() == 0
+    assert actual.direct_damage[actual.outcome == 0].sum() == 0
+    assert actual.direct_damage[actual.outcome == 1].min() == 1 + 3
+    assert actual.direct_damage[actual.outcome == 1].max() == 6 + 3
+    assert actual.direct_damage[actual.outcome == 2].min() == (1 + 3) * 2 + 1
+    assert actual.direct_damage[actual.outcome == 2].max() == (6 + 3) * 2 + 8
+
+    assert_equal(actual.direct_damage.sum("damage_type"), actual.total_damage)
+    assert "splash_damage" not in actual
+    assert "persistent_damage" not in actual
+    assert "weaknesses" not in actual
+    assert "resistances" not in actual
+    assert "immunities" not in actual
+
+    assert actual.attrs["damage_spec"] == {
+        "Success": "1d6+3 slashing",
+        "Critical success": "(1d6+3)x2 slashing plus 1d8 slashing",
+    }
+
+
+def test_no_damage():
+    actual = damage(check(16, DC=21), Damage("slashing", 0, 0, 0))
+    assert actual.total_damage.sizes == {"roll": 1000}
+    assert actual.total_damage.sum() == 0
+    assert "initial_damage" not in actual
+    assert "splash_damage" not in actual
+    assert "persistent_damage" not in actual
+
+
+@pytest.mark.parametrize("bonus", [0, 1])
+def test_no_damage_consistent_shape(bonus):
+    actual = damage(
+        check(
+            16,
+            DC=DataArray([20, 22, 24], dims=["target"]),
+            dims={"foo": 2},
+        ),
+        Damage("slashing", 0, 0, bonus),
+    )
+    assert actual.total_damage.sizes == {"roll": 1000, "target": 3, "foo": 2}
+    assert actual.total_damage.min() == 0
+    assert actual.total_damage.max() == bonus * 2
+
+
+def test_splash_damage():
+    actual = damage(
+        check(6, DC=15),
+        Damage("fire", 1, 8) + Damage("fire", 0, 0, 1, splash=True),
+        splash_damage_targets=4,
+    )
+    assert actual.total_damage[actual.outcome == -1].sum() == 0
+    assert actual.total_damage[actual.outcome == 0].min() == 4
+    assert actual.total_damage[actual.outcome == 0].max() == 4
+
+    # Main target splash has been added to direct damage
+    assert actual.direct_damage[actual.outcome == 0].min() == 1
+    assert actual.direct_damage[actual.outcome == 0].max() == 1
+    assert actual.splash_damage[actual.outcome == 0].min() == 1
+    assert actual.splash_damage[actual.outcome == 0].max() == 1
+
+    assert actual.total_damage[actual.outcome == 1].min() == 1 + 4
+    assert actual.total_damage[actual.outcome == 1].max() == 8 + 4
+    assert actual.total_damage[actual.outcome == 2].min() == 1 * 2 + 4
+    assert actual.total_damage[actual.outcome == 2].max() == 8 * 2 + 4
+
+
+def test_splash_damage_no_direct():
+    actual = damage(
+        check(6, DC=15),
+        Damage("fire", 0, 0, 1, splash=True),
+    )
+    assert actual.total_damage[actual.outcome == -1].sum() == 0
+
+    # Main target splash has been added to direct damage, even if there's no
+    # direct damage anyway
+    assert actual.direct_damage[actual.outcome > -1].min() == 1
+    assert actual.direct_damage[actual.outcome > -1].max() == 1
+    assert actual.splash_damage[actual.outcome > -1].min() == 1
+    assert actual.splash_damage[actual.outcome > -1].max() == 1
+    assert actual.total_damage[actual.outcome > -1].min() == 2
+    assert actual.total_damage[actual.outcome > -1].max() == 2
+
+
+def test_persistent_damage():
+    actual = damage(check(6, DC=15), Damage("fire", 1, 6, persistent=True))
+    assert actual.attrs["persistent_damage_DC"] == 15
+    assert "direct_damage" not in actual
+    assert "splash_damage" not in actual
+    assert actual.persistent_damage.sizes == {
+        "roll": 1000,
+        "damage_type": 1,
+        "persistent_round": 3,
+    }
+    assert actual.total_damage.sizes == {"roll": 1000}
+
+    assert actual.persistent_damage[actual.outcome > 0].min() == 1
+    assert actual.persistent_damage.max() == 12
+    assert actual.persistent_damage_check.min() == 0
+    assert actual.persistent_damage_check.max() == 1
+    assert actual.apply_persistent_damage.isel(persistent_round=0).all()
+    assert 0.65 < actual.apply_persistent_damage.isel(persistent_round=1).mean() < 0.75
+    assert 0.44 < actual.apply_persistent_damage.isel(persistent_round=2).mean() < 0.54
+
+    assert actual.total_damage[actual.outcome <= 0].sum() == 0
+    assert actual.total_damage[actual.outcome == 1].min() == 1
+    assert actual.total_damage[actual.outcome == 2].min() == 2
+    assert 16 < actual.total_damage[actual.outcome == 1].max() <= 18
+    assert 32 < actual.total_damage[actual.outcome == 2].max() <= 36
+
+
+def test_persistent_damage_DC20():
+    actual = damage(
+        check(6, DC=15),
+        Damage("bleed", 0, 0, 1, persistent=True),
+        persistent_damage_DC=20,
+    )
+    assert actual.persistent_damage_check.min() == 0
+    assert actual.persistent_damage_check.max() == 1
+    assert 0.03 < actual.persistent_damage_check.mean() < 0.07
+    assert actual.apply_persistent_damage.isel(persistent_round=0).all()
+    assert 0.93 < actual.apply_persistent_damage.isel(persistent_round=1).mean() < 0.97
+    assert 0.88 < actual.apply_persistent_damage.isel(persistent_round=2).mean() < 0.92
+    assert 2.7 < actual.total_damage[actual.outcome == 1].mean() < 2.9
+
+
+def test_persistent_damage_DC10():
+    actual = damage(
+        check(6, DC=15),
+        Damage("bleed", 0, 0, 1, persistent=True),
+        persistent_damage_DC=10,
+    )
+    assert actual.persistent_damage_check.min() == 0
+    assert actual.persistent_damage_check.max() == 1
+    assert 0.52 < actual.persistent_damage_check.mean() < 0.57
+    assert actual.apply_persistent_damage.isel(persistent_round=0).all()
+    assert 0.42 < actual.apply_persistent_damage.isel(persistent_round=1).mean() < 0.47
+    assert 0.18 < actual.apply_persistent_damage.isel(persistent_round=2).mean() < 0.22
+    assert 1.5 < actual.total_damage[actual.outcome == 1].mean() < 1.9
+
+
+@pytest.mark.parametrize(
+    "immunities",
+    [
+        ["fire"],
+        ("fire", "cold"),
+        {"fire": True},
+        {"fire": True, "cold": True, "slashing": False},
+        DataArray(
+            [True, True],
+            dims=["damage_type"],
+            coords={"damage_type": ["cold", "fire"]},
+        ),
+    ],
+)
+def test_immunities(immunities):
+    actual = damage(
+        check(6, DC=15),
+        Damage("slashing", 1, 6) + Damage("fire", 0, 0, 1),
+        immunities=immunities,
+    )
+    assert actual.immunities.dtype == bool
+    assert_equal(
+        actual.immunities,
+        DataArray(
+            [True, False],
+            dims=["damage_type"],
+            coords={"damage_type": ["fire", "slashing"]},
+        ),
+    )
+    assert actual.direct_damage.sel(damage_type="slashing").max() == 12
+    assert actual.direct_damage.sel(damage_type="fire").max() == 0
+
+
+def test_weaknesses():
+    actual = damage(
+        check(6, DC=15),
+        Damage("fire", 0, 0, 1),
+        weaknesses={"fire": 10},
+    )
+    # Weaknesses don't double on a crit
+    assert np.unique(actual.total_damage).tolist() == [0, 11, 12]
+
+
+def test_resistances():
+    actual = damage(
+        check(6, DC=15),
+        Damage("fire", 1, 6),
+        resistances={"fire": 5},
+    )
+    assert np.unique(actual.total_damage[actual.outcome == 1]).tolist() == [0, 1]
+    # Resistances don't double on a crit
+    assert actual.total_damage[actual.outcome == 2].max() == 7
+
+
+def test_weaknesses_splash():
+    """Splash damage adds to direct before calculating weaknesses"""
+    actual = damage(
+        check(6, DC=15),
+        Damage("fire", 0, 0, 1) + Damage("fire", 0, 0, 1, splash=True),
+        weaknesses={"fire": 10},
+        splash_damage_targets=3,
+    )
+    assert (actual.total_damage[actual.outcome == 1] == 34).all()
+
+
+def test_resistances_splash():
+    """Splash damage adds to direct before calculating resistances"""
+    actual = damage(
+        check(6, DC=15),
+        Damage("fire", 0, 0, 4) + Damage("fire", 0, 0, 3, splash=True),
+        resistances={"fire": 5},
+        splash_damage_targets=9,
+    )
+    # 2 damage to main target, 0 to others
+    assert (actual.total_damage[actual.outcome == 1] == 2).all()
+
+
+def test_weaknesses_persistent():
+    """Weaknesses re-apply at every application of persistent damage"""
+    actual = damage(
+        check(6, DC=15),
+        Damage("fire", 0, 0, 1) + Damage("fire", 0, 0, 1, persistent=True),
+        weaknesses={"fire": 10},
+    )
+    assert actual.total_damage[actual.outcome == 1].min() == 22  # at least 1 round
+    assert actual.total_damage[actual.outcome == 2].min() == 24
+    assert actual.total_damage[actual.outcome == 1].max() == 44  # 2 failed saves
+    assert actual.total_damage[actual.outcome == 2].max() == 48
+
+
+def test_resistances_persistent():
+    """Resistances re-apply at every application of persistent damage"""
+    set_size(5_000)
+
+    actual = damage(
+        check(6, DC=15),
+        Damage("fire", 1, 6) + Damage("fire", 1, 6, persistent=True),
+        resistances={"fire": 5},
+    )
+    assert actual.direct_damage[actual.outcome == 1].max() == 1
+    assert_equal(
+        actual.persistent_damage[actual.outcome == 1].max(["roll", "damage_type"]),
+        DataArray([1, 1, 1], dims=["persistent_round"]),
+    )
+    assert actual.total_damage[actual.outcome > 0].min() == 0
+    assert actual.total_damage[actual.outcome == 1].max() == 4  # 2 failed saves
+    assert 25 < actual.total_damage[actual.outcome == 2].max() <= 28
+
+
+def test_apply_weakness_once():
+    """Multiple sources of immediate damage off the same type trigger weaknesses and
+    resistances only once
+    """
+    actual = damage(
+        check(6, DC=15),
+        Damage("slashing", 1, 6, deadly=8),
+        weaknesses={"slashing": 10},
+    )
+    assert actual.total_damage[actual.outcome == 2].min() == 1 * 2 + 1 + 10
+    assert actual.total_damage.max() == 6 * 2 + 8 + 10
+
+
+def test_apply_resistance_once():
+    """Multiple sources of immediate damage off the same type trigger weaknesses and
+    resistances only once
+    """
+    actual = damage(
+        check(6, DC=15),
+        Damage("slashing", 1, 6, deadly=8),
+        resistances={"slashing": 10},
+    )
+    assert actual.total_damage[actual.outcome == 2].min() == 0
+    assert actual.total_damage.max() == 6 * 2 + 8 - 10
+
+
+@pytest.mark.parametrize("key", ["weaknesses", "resistances", "immunities"])
+def test_weaknesses_array(key):
+    actual = damage(
+        check(6, DC=15),
+        Damage("slashing", 1, 6, deadly=8),
+        **{
+            key: DataArray(
+                [[1, 0], [0, 1]],
+                dims=["damage_type", "target"],
+                coords={"damage_type": ["fire", "slashing"], "target": ["foo", "bar"]},
+            )
+        },
+    )
+    assert actual.total_damage.sizes == {"roll": 1000, "target": 2}
+    assert actual.target.values.tolist() == ["foo", "bar"]
+
+
+@pytest.mark.parametrize("key", ["weaknesses", "resistances", "immunities"])
+def test_malformed_weaknesses(key):
+    with pytest.raises(ValueError):
+        damage(
+            check(6, DC=15),
+            Damage("slashing", 1, 6),
+            **{key: {"fire": 0.5}},
+        )
+
+    with pytest.raises(ValueError):
+        damage(
+            check(6, DC=15),
+            Damage("slashing", 1, 6),
+            **{key: DataArray(1)},
+        )
+
+
+def test_basic_save():
+    """Halving the damage on a basic save rounds down to integer"""
+    actual = damage(check(6, DC=15), Damage("fire", 1, 4, basic_save=True))
+    assert actual.direct_damage.dtype.kind == "i"
+    assert actual.total_damage.dtype.kind == "i"
+    assert actual.total_damage[actual.outcome == -1].max() == 8
+    assert actual.total_damage[actual.outcome == 0].max() == 4
+    assert np.unique(actual.total_damage[actual.outcome == 1]).tolist() == [0, 1, 2]
+    assert actual.total_damage[actual.outcome == 2].max() == 0
