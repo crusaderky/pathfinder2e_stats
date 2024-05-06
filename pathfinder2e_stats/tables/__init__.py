3c3c1a844057c72a596e72ab9560d202d4737ae6
crusaderky <crusaderky@gmail.com>
2024-05-06 12:47:21 +0100

    NPC tables (#29)

--- pathfinder2e_stats/tables/__init__.py
+++ pathfinder2e_stats/tables/__init__.py
@@ -3,12 +3,20 @@ from __future__ import annotations
 from pathlib import Path
 
 import pandas as pd
-from xarray import DataArray, Dataset
+from xarray import DataArray, Dataset, align, concat
 
 
-def _read_raw_tables(path: str) -> Dataset:
+def _ensure_var_dtypes(ds: Dataset) -> None:
+    for k, var in ds.variables.items():
+        if var.dtype == object:
+            ds[k] = var.astype("U")
+        else:
+            assert var.dtype.kind in ("i", "b"), var
+
+
+def _read_PC_tables() -> Dataset:
     data_vars = {}
-    fnames = (Path(__file__).parent / path).glob("*.csv")
+    fnames = (Path(__file__).parent / "PC").glob("*.csv")
 
     for fname in sorted(fnames):
         df = pd.read_csv(fname, index_col=0).ffill().fillna(0).astype(int)
@@ -16,13 +24,66 @@ def _read_raw_tables(path: str) -> Dataset:
         data_vars[name] = DataArray(df).rename({"dim_1": name + "_col"})
 
     ds = Dataset(data_vars=data_vars)
-    for k, coord in ds.coords.items():
-        if coord.dtype == object:
-            ds.coords[k] = coord.astype("U")
+    _ensure_var_dtypes(ds)
     return ds
 
 
-raw_PC = _read_raw_tables("PC")
-raw_NPC = _read_raw_tables("NPC")
+def _read_NPC_table(fname: Path) -> DataArray:
+    df = pd.read_csv(
+        fname,
+        index_col=0,
+        header=[0, 1] if fname.name == "HP.csv" else 0,
+    )
 
-__all__ = ("raw_PC", "raw_NPC")
+    arr = DataArray(df)
+
+    dim_1 = arr.coords["dim_1"]
+    if fname.name == "HP.csv":
+        arr = arr.unstack("dim_1", fill_value=1337)
+        # Undo alphabetical sorting
+        arr = arr.sel(challenge=["High", "Moderate", "Low"])
+    elif "High" in dim_1:
+        arr = arr.rename({"dim_1": "challenge"})
+    elif "max" in dim_1:
+        arr = arr.rename({"dim_1": "mm"})
+    elif dim_1[0] == "Unlimited":
+        arr = arr.rename({"dim_1": "limited"})
+        arr.coords["limited"] = [False, True]
+    else:
+        raise AssertionError("unreachable")  # pragma: nocover
+
+    if "mm" in arr.dims:
+        mean = arr.sum("mm").expand_dims(mm=["mean"]) / 2
+        mean = mean.round(0).astype(int)
+        arr = concat([arr, mean], dim="mm")
+
+    return arr
+
+
+def _read_NPC_tables() -> Dataset:
+    names = []
+    vars = []
+    fnames = (Path(__file__).parent / "NPC").glob("*.csv")
+
+    for fname in sorted(fnames):
+        names.append(fname.name.removesuffix(".csv"))
+        vars.append(_read_NPC_table(fname))
+
+    vars = list(align(*vars, join="outer", fill_value=0))
+
+    ds = Dataset(data_vars=dict(zip(names, vars, strict=True)))
+    _ensure_var_dtypes(ds)
+    # Restore priority order after align
+    ds = ds.sortby(
+        DataArray(
+            ["Extreme", "High", "Moderate", "Low", "Terrible"],
+            dims=["challenge"],
+        )
+    )
+    return ds
+
+
+PC = _read_PC_tables()
+NPC = _read_NPC_tables()
+
+__all__ = ("PC", "NPC")
--- pathfinder2e_stats/tests/test_tables.py
+++ pathfinder2e_stats/tests/test_tables.py
@@ -1,13 +1,8 @@
-import pytest
-
-from pathfinder2e_stats import tables
+from pathfinder2e_stats import roll, tables
 
 
-@pytest.mark.parametrize(
-    "name", ["raw_PC", pytest.param("raw_NPC", marks=pytest.mark.xfail(reason="TODO"))]
-)
-def test_raw(name):
-    ds = getattr(tables, name)
+def test_PC():
+    ds = tables.PC
     assert ds.data_vars
     for k, v in ds.variables.items():
         if k in ds.data_vars or k == "level":
@@ -19,3 +14,40 @@ def test_raw(name):
     assert ds["weapon_proficiency"].sel(level=6, weapon_proficiency_col="Fighter") == 6
     # test fill with zeros
     assert ds["attack_item_bonus"].sel(level=1, attack_item_bonus_col="Bomb") == 0
+
+
+def test_NPC():
+    ds = tables.NPC
+    assert set(ds.dims) == {"level", "challenge", "mm", "limited"}
+
+    assert ds.data_vars
+    for v in ds.data_vars.values():
+        if v.dtype.kind == "U":
+            # Test that text is a well-formed dice expression, e.g. 2d6+1
+            for dice in v.values.flat:
+                if dice != "0":  # Fill-in for terrible
+                    roll(dice)  # TODO separate pareser from roller
+        else:
+            assert v.dtype.kind == "i"
+            if "mm" in v.dims:
+                assert (v.sel(mm="max") >= v.sel(mm="min")).all()
+
+    # Test that coords have not been reordered alphabetically
+    assert ds.challenge.values.tolist() == [
+        "Extreme",
+        "High",
+        "Moderate",
+        "Low",
+        "Terrible",
+    ]
+
+    # Test that mean uses mathematical rounding and not truncation
+    assert ds.resistances.sel(level=2).values.tolist() == [5, 2, 4]
+
+    # Test that Extreme and Terrible are filled with zeros when missing
+    assert ds.HP.sel(level=2, mm="min").values.tolist() == [0, 36, 28, 21, 0]
+
+    # Test that unstack didn't need to use fill values
+    HP = ds.HP.sel(challenge=["High", "Moderate", "Low"])
+    assert (HP > 0).all()
+    assert (HP < 700).all()
