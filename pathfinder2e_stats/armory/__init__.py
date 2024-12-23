84ee2dd8741ee1c0b39b07d21ebf0d4bce7a9a8f
crusaderky <crusaderky@gmail.com>
2024-12-23 12:00:47 +0000

    Revert "revert"
    This reverts commit 7252eeff4ce8b11c1ba9c67a8d0759c789a290a7.

---
+++ pathfinder2e_stats/armory/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import pathfinder2e_stats.armory.cantrips
+import pathfinder2e_stats.armory.picks
+import pathfinder2e_stats.armory.runes
+import pathfinder2e_stats.armory.spells
+import pathfinder2e_stats.armory.swords  # noqa: F401
