e1b89083d4766393b1246a6b311cc5d4682fa32c
crusaderky <crusaderky@gmail.com>
2025-08-02 22:45:14 +0100

    Armory: crossbows tweaks

--- pathfinder2e_stats/armory/crossbows.py
+++ pathfinder2e_stats/armory/crossbows.py
@@ -6,18 +6,22 @@ from pathfinder2e_stats.damage_spec import Damage, ExpandedDamage
 
 __all__ = (
     "arbalest",
+    "crescent_cross",
     "critical_specialization",
     "crossbow",
+    "gauntlet_bow",
     "hand_crossbow",
     "heavy_crossbow",
     "sukgung",
 )
 
 
-hand_crossbow = _weapon("hand_crossbow", "piercing", 6)
-crossbow = _weapon("crossbow", "piercing", 8)
-heavy_crossbow = _weapon("heavy_crossbow", "piercing", 10)
 arbalest = _weapon("arbalest", "piercing", 10)
+crescent_cross = _weapon("crescent_cross", "piercing", 6)
+crossbow = _weapon("crossbow", "piercing", 8)
+gauntlet_bow = _weapon("gauntlet_bow", "piercing", 4)
+hand_crossbow = _weapon("hand_crossbow", "piercing", 6)
+heavy_crossbow = _weapon("heavy_crossbow", "piercing", 10)
 sukgung = _weapon("sukgung", "piercing", 8, fatal_aim=12)
 
 
@@ -27,5 +31,5 @@ def critical_specialization(item_attack_bonus: int) -> ExpandedDamage:
     The target takes 1d6 persistent bleed damage. You gain an item bonus to this
     bleed damage equal to the weapon's item bonus to attack rolls.
     """
-    base = Damage("bleed", 1, 6, item_attack_bonus, persistent=True)
+    base = Damage("bleed", 1, 8, item_attack_bonus, persistent=True)
     return ExpandedDamage({DoS.critical_success: [base]})
