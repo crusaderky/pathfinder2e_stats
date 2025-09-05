79dea1bca7a085bda71be16d07c64f67e1771764
crusaderky <crusaderky@gmail.com>
2025-09-05 13:18:26 +0100

    armory.starfinder.axes

--- pathfinder2e_stats/armory/starfinder/axe.py
+++ pathfinder2e_stats/armory/starfinder/axe.py
@@ -1,1 +1,12 @@
-__all__ = ()
+from pathfinder2e_stats.armory._common import _weapon
+
+__all__ = ("doshko", "fangblade", "plasma_doshko")
+
+doshko = _weapon("doshko", "piercing", 12)
+fangblade = _weapon("fangblade", "slashing", 10, boost=12)
+plasma_doshko = _weapon("plasma_doshko", "fire", 10)
+
+plasma_doshko.__doc__ = """
+:func:`Critical (plasma)
+<pathfinder2e_stats.armory.critical_specialization.plasma>`
+"""
