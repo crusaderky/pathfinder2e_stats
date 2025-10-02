f59e043955b8465722ea72f929e9c4eba1aefc31
Guido Imperiale <crusaderky@gmail.com>
2025-10-02 23:36:29 +0200

    Reinstate MacIntel CI (#161)

--- smoke_test.py
+++ smoke_test.py
@@ -1,10 +1,12 @@
 """Test import the library and print essential information"""
 
+import platform
 import sys
 
 import pathfinder2e_stats
 
 print("Python interpreter:", sys.executable)
 print("Python version    :", sys.version)
+print("Platform          :", platform.platform())
 print("Library path      :", pathfinder2e_stats.__file__)
 print("Library version   :", pathfinder2e_stats.__version__)
